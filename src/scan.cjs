f08f9696743e50b3be061649b6e3e06bebaee37e
Carter Medlin <carterm@users.noreply.github.com>
2024-03-07 14:15:24 -0800

    works nice

--- src/scan.cjs
+++ src/scan.cjs
@@ -4,15 +4,18 @@ const http = require("node:http");
 const https = require("node:https");
 const fs = require("node:fs");
 const { JSDOM } = require("jsdom");
+const { URL } = require("node:url");
 
 const urls = fs
   .readFileSync(`${__dirname}/targets.txt`, {
     encoding: "utf-8"
   })
-  .split("\n");
+  .split("\n")
+  .filter(x => x);
 
 /**
  * @param {string} url
+ * @returns {Promise<string>}
  */
 const getScript = url => {
   return new Promise((resolve, reject) => {
@@ -34,24 +37,35 @@ const getScript = url => {
   });
 };
 
+// Process the results (e.g., extract JavaScript links)
 const processUrls = async () => {
   const results = await Promise.all(
-    urls.map(u =>
-      getScript(u)
+    urls.map(target =>
+      getScript(target)
         .then(response => {
           const dom = new JSDOM(response);
-          const links = dom.window.document.querySelectorAll("a");
 
-          links.forEach(link => {
-            console.log(link.href);
-          });
+          const scripts = [...dom.window.document.scripts]
+            .map(x => x.src)
+            .filter(x => x)
+            .map(x => new URL(x, target).href);
+
+          const stateTemplate = scripts.find(x => x.includes("cagov.core"));
+          const JQuery = scripts.find(x => x.includes("jquery"));
+
+          return {
+            target,
+            scripts,
+            statewideAlerts: scripts.includes("https://alert.cdt.ca.gov/"),
+            stateTemplate,
+            JQuery
+          };
         })
         .catch(err => {
           console.error(err);
         })
     )
   );
-  // Process the results (e.g., extract JavaScript links)
 
   // Save results to a JSON file
   const jsonResults = JSON.stringify(results, null, 2);
