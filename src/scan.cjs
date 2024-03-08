756b4d61991007432e60ecc780fee45bd327900a
Carter Medlin <carterm@users.noreply.github.com>
2024-03-07 16:22:54 -0800

    working

--- src/scan.cjs
+++ src/scan.cjs
@@ -4,20 +4,29 @@ const fs = require("node:fs");
 const { JSDOM } = require("jsdom");
 const { URL } = require("node:url");
 
-const urls = fs
-  .readFileSync(`${__dirname}/targets.txt`, {
-    encoding: "utf-8"
-  })
-  .split("\n")
-  .map(x => x.trim())
-  .filter(x => x.startsWith("http"));
+const urls = [
+  ...new Set(
+    fs
+      .readFileSync(`${__dirname}/targets.txt`, {
+        encoding: "utf-8"
+      })
+      .split("\n")
+      .map(x => x.trim())
+      .filter(x => !x.startsWith("//")) //remove comments
+      .map(x => (x.includes("/") ? x : `${x}/`))
+      .map(x => (x.startsWith("http") ? x : `https://${x}`))
+  )
+].sort();
+
+console.log(`Processing ${urls.length} urls...`);
 
 // Process the results (e.g., extract JavaScript links)
 const processUrls = async () => {
   const results = await Promise.all(
     urls.map(target =>
-      JSDOM.fromURL(target, {})
+      JSDOM.fromURL(target)
         .then(dom => {
+          console.log(`${target}...reading`);
           const scripts = [...dom.window.document.scripts]
             .map(x => x.src)
             .filter(x => x)
@@ -35,7 +44,11 @@ const processUrls = async () => {
         })
         .catch(error => {
           // console.error(target, error);
+          console.log(`${target}...error.`);
           return { target, error: { message: error.message } };
+        })
+        .finally(() => {
+          console.log(`${target}...done.`);
         })
     )
   );
@@ -46,5 +59,8 @@ const processUrls = async () => {
 
   fs.writeFileSync("results.json", jsonResults);
 };
+(async () => {
+  await processUrls();
 
-processUrls();
+  console.log(`done.`);
+})();
