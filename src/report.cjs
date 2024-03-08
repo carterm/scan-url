28421d3eb262ee6e87581785c1a82b7a3069bffc
Carter Medlin <carterm@users.noreply.github.com>
2024-03-08 13:36:59 -0800

    better domain list after redirects

--- src/report.cjs
+++ src/report.cjs
@@ -14,6 +14,24 @@ console.time("Done");
   };
   fs.writeFileSync("_results/report.json", JSON.stringify(result, null, 2));
 
+  const newList = [
+    ...new Set(
+      data.map(x => {
+        if (x.redirectURL && x.redirectURL.endsWith("/")) {
+          if (x.redirectURL?.split("/").length == 4) {
+            return x.redirectURL;
+          }
+        }
+
+        return x.target;
+      })
+    )
+  ];
+
+  newList.sort();
+
+  fs.writeFileSync("_results/resort.txt", newList.join("\n"));
+
   console.timeEnd("Done");
   process.exit();
 })();
