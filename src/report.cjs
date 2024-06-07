726dfc47a6f00f51c7cc0de5035746d7be1b6e57
Carter Medlin <carterm@users.noreply.github.com>
2024-06-07 13:58:42 -0700

    removing timeouts

--- src/report.cjs
+++ src/report.cjs
@@ -4,37 +4,23 @@ const fs = require("node:fs");
 
 console.time("Done");
 
-const data = /** @type {*[]} */ (require("../_results/results.json"));
+const data = require("../_results/results.json");
 
 (() => {
-  const alertsUsers = data.filter(x => x.statewideAlerts).map(x => x.target);
-
-  const result = {
-    total: data.length,
-    alertCount: data.filter(x => x.statewideAlerts).length,
-    stateTemplateCount: data.filter(x => x.stateTemplate).length,
-    GoogleAnalytics: data.filter(x => x.GoogleAnalytics).length,
-    GoogleAnalytics_G: data.filter(x =>
-      x.GoogleAnalytics?.some(y => y.startsWith("G-"))
-    ).length,
-    GoogleAnalytics_UA: data.filter(x =>
-      x.GoogleAnalytics?.some(y => y.startsWith("UA-"))
-    ).length,
-    alertsUsers
-  };
-  fs.writeFileSync("_results/report.json", JSON.stringify(result, null, 2));
-
   let newList = [
     ...new Set(
-      data.map(x => {
-        if (x.redirectURL && x.redirectURL.endsWith("/")) {
-          if (x.redirectURL?.split("/").length == 4) {
-            return x.redirectURL;
-          }
-        }
+      data
+        .filter(x => !["ETIMEDOUT", "ENOTFOUND"].includes(x.error?.code || ""))
 
-        return x.target;
-      })
+        .map(x => {
+          if (x.redirectURL && x.redirectURL.endsWith("/")) {
+            if (x.redirectURL?.split("/").length == 4) {
+              return x.redirectURL;
+            }
+          }
+
+          return x.target;
+        })
     )
   ];
   newList = newList.filter(
@@ -44,7 +30,6 @@ const data = /** @type {*[]} */ (require("../_results/results.json"));
   newList.sort();
 
   fs.writeFileSync("_results/resort.txt", newList.join("\n"));
-  console.log(result);
 
   console.timeEnd("Done");
   process.exit();
