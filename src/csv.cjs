f9083a4aab1b5c3066ec58608c563ed847d913ee
Carter Medlin <carterm@users.noreply.github.com>
2025-04-18 11:41:57 -0700

    domain sorting

--- src/csv.cjs
+++ src/csv.cjs
@@ -11,8 +11,8 @@ const data = require("../_results/results.json");
 (() => {
   const resultData = [
     {
+      domain: 0,
       target: data.length,
-      domain: data.length,
       redirectURL: data.filter(x => x.redirectURL).length,
       title: data.filter(x => x.title).length,
       generator: data.filter(x => x.generator).length,
@@ -23,20 +23,36 @@ const data = require("../_results/results.json");
       errorcode: data.filter(x => x.error?.code).length,
       errormessage: data.filter(x => x.error?.message).length
     },
-    ...data.map(row => ({
-      target: row.target,
-      domain: (row.target.match(/\W*(\w*\.ca\.gov)/) || [])[1] || "",
-      redirectURL: row.redirectURL || "",
-      title: row.title || "",
-      generator: row.generator || "",
-      statewideAlerts: row.statewideAlerts || "",
-      stateTemplate: row.stateTemplate || "",
-      JQuery: row.JQuery || "",
-      GoogleAnalytics: row.GoogleAnalytics ? row.GoogleAnalytics.join(",") : "",
-      errorcode: row.error?.code || "",
-      errormessage: row.error?.message || ""
-    }))
+    ...data
+      .map(row => ({
+        domain: (row.target.match(/\W*([\w-]*\.ca\.gov)/) || [])[1] || "",
+        target: row.target,
+        redirectURL: row.redirectURL || "",
+        title: row.title || "",
+        generator: row.generator || "",
+        statewideAlerts: row.statewideAlerts || "",
+        stateTemplate: row.stateTemplate || "",
+        JQuery: row.JQuery || "",
+        GoogleAnalytics: row.GoogleAnalytics
+          ? row.GoogleAnalytics.join(",")
+          : "",
+        errorcode: row.error?.code || "",
+        errormessage: row.error?.message || ""
+      }))
+      .sort((a, b) => {
+        if (a.domain > b.domain) return 1;
+        if (a.domain < b.domain) return -1;
+        if (a.domain === b.domain) {
+          if (a.target > b.target) return 1;
+          if (a.target < b.target) return -1;
+        }
+        return 0;
+      })
   ];
+
+  resultData[0].domain = new Set(
+    resultData.slice(1).map(row => row.domain)
+  ).size;
 
   const csv = converter.json2csv(resultData);
 
